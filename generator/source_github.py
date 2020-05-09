1b77d4756384186f46f4194ffb50e20bbb3fdaf0
Eli2 <FabianRoth64@googlemail.com>
2020-05-09 05:14:05 +0200

    Github backend

--- generator/source_github.py
+++ generator/source_github.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2020, Eli2
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from github3 import GitHub
-from github3.session import GitHubSession
-from github3.exceptions import GitHubError, NotFoundError
+import os
+import json
+import requests
+from sgqlc.endpoint.http import HTTPEndpoint
 
 import base64
 
@@ -13,76 +14,194 @@ from .common import g_log
 from .source import ModDataFile, RepositorySource, Repository
 
 
+# https://github.com/profusion/sgqlc
+
 class GitHubSource(RepositorySource):
-	
+
 	def __init__(self):
-		self.gh = GitHub()
+
+		gh_token = os.environ['GH_TOKEN']
+		if gh_token:
+			headers = {'Authorization': 'bearer ' + gh_token}
+		else:
+			headers = None
+			
+		self._headers = headers
+		
+		self.endpoint = HTTPEndpoint('https://api.github.com/graphql', headers)
 
 	def list_mods(self):
-		org = self.gh.organization('nextmod')
-		repos = org.repositories()
-		for repo in repos:
-			try:
-				repo.file_contents('mod-info.md')
-			except NotFoundError as ex:
-				g_log.info(f'mod-info file not found in repository {repo.name}, skipping')
+		query = '''
+		query {
+			organization(login:"nextmod") {
+				repositories(first: 10) {
+					nodes{
+						name
+					}
+				}
+			}
+		}'''
+		data = self.endpoint(query)
+
+		for repo in data['data']['organization']['repositories']['nodes']:
+			repo_name = repo['name']
+
+			if not repo_name.startswith('mw_'):
+				g_log.info(f'Unexpected repo prefix {repo_name} skipping')
 				continue
-				
-			p = GitHubRepository()
-			p.id = repo.name
-			p.name = repo.name
-			p._repo = repo
-			yield p
+
+			game_id, mod_id = repo_name.split('_', 1)
+
+			yield GitHubRepository(self,  self.endpoint, 'nextmod', repo_name, game_id, mod_id)
 
 
 class GitHubRepository(Repository):
-	id = ''
-	name = ''
-	_repo = None
+	
+	_repo_owner: str
+	_repo_name: str
+
+	game_id: str
+	mod_id: str
+
+	def __init__(self, src, endpoint, repo_owner, repo_name, game_id, mod_id):
+		self._src = src
+		self.endpoint = endpoint
+		self._repo_owner = repo_owner
+		self._repo_name = repo_name
+
+		self.game_id = game_id
+		self.mod_id = mod_id
 
 	def list_dir(self, dir_path):
-		path_str = str(dir_path)
-		try:
-			files = self._repo.directory_contents(dir_path)
-			for f in files:
-				yield f['name']
-		except GitHubError as ex:
-			g_log.error("GitHub failed to list files: %s", ex)
+		dir_path = str(dir_path)
+		if not dir_path.endswith('/'):
+			dir_path += '/'
+		
+		query = '''
+		query ($repo_owner: String!, $repo_name: String!, $expression: String!) {
+		  repository(owner: $repo_owner, name: $repo_name) {    
+			object(expression: $expression) {
+			  ... on Tree {
+				entries {
+				  name
+				  type
+				}
+			  }
+			}
+		  }
+		}
+		'''
+		variables = {
+			'repo_owner': self._repo_owner,
+			'repo_name': self._repo_name,
+			'expression': 'master:' + dir_path
+		}
+		data = self.endpoint(query, variables)
+
+		obj = data['data']['repository']['object']
+		if not obj:
+			return []
+		
+		entries = obj['entries']
+		names = [e['name'] for e in entries]
+		return names
 
 	def list_data_files(self):
-		data_path = "data/"
-		try:
-			result = []
-			files = self._repo.directory_contents(data_path)
-			for f in files:
-				pass
-				# TODO
-				"""
-				if f['type'] == 'tree':
-					continue
-				blob_id = f['id']
-				path = f['path']
-				file_info = self.p_project.repository_blob(blob_id)
-				size = file_info['size']
-				result.append(ModDataFile(path, size))
-				"""
-			return tuple(result)
-		except GitHubError as ex:
-			g_log.error("GitHub failed to list files: %s", ex)
+		dir_path = 'data/'
+		
+		def gen_tree(depth: int):
+			if depth == 0:
+				return ''
+			return 'object {...on Blob{byteSize} ... on Tree {entries{name type ' + gen_tree(depth - 1) + '}}}'
+
+		def gen_tree_frag():
+			return '... on Tree {entries{name type ' + gen_tree(8) + '}}'
+
+		query = '''
+		query ($repo_owner: String!, $repo_name: String!, $expression: String!) {
+		  repository(owner: $repo_owner, name: $repo_name) {
+			object(expression: $expression) {
+			''' + gen_tree_frag() + '''
+			}
+		  }
+		}
+		'''
+		
+		variables = {
+			'repo_owner': self._repo_owner,
+			'repo_name': self._repo_name,
+			'expression': 'master:' + dir_path
+		}
+		data = self.endpoint(query, variables)
+		
+		root_obj = data['data']['repository']['object']
+		if root_obj:
+			flat = []
+			
+			def walk(obj, prefix):
+				for e in obj['entries']:
+					if e['type'] == 'tree':
+						walk(e['object'], prefix + '/' + e['name'])
+					elif e['type'] == 'blob':
+						flat.append(('data' + prefix + '/' + e['name'], e['object']['byteSize']))
+
+			
+			walk(root_obj, '')
+			return flat
+		else:
 			return []
 
 	def get_file(self, file_path: PurePath):
-		path_str = str(file_path)
+		
+		query = '''
+		query($owner:String!, $name:String!, $exp:String!) {
+			repository(owner: $owner, name: $name) {
+				object(expression: $exp) {
+					... on Blob {
+						oid
+					}
+				}	
+			}
+		}'''
+		variables = {
+			'owner': self._repo_owner,
+			'name': self._repo_name,
+			'exp': 'master:' + str(file_path)
+		}
+		data = self.endpoint(query, variables)
+		
+		oid = data['data']['repository']['object']['oid']
+		
+		# No clue how to do this with API v4, use v3 (2020.05)
+
+		foo = ["https://api.github.com", 'repos', self._repo_owner, self._repo_name, 'git', 'blobs', oid]
+		bar = '/'.join(foo)
+		
 		try:
-			file_contents = self._repo.file_contents(path_str)
-			file_data = base64.decodebytes(bytes(file_contents.content, 'UTF-8'))
-			return file_data
-		except GitHubError as ex:
-			g_log.error("gitlab failed to get file %s: %s", file_path, ex)
+			r = requests.get(bar, headers=self._src._headers)
+			r.raise_for_status()
+			j = r.json()
+			if j['encoding'] == 'base64':
+				data = base64.b64decode(j['content'])
+			else:
+				g_log.error("Unexpected encoding")
+			return data
+		except Exception as ex:
+			g_log.exception(ex)
 			return None
+	
+
 
 	def get_star_count(self):
-		i = 0
-		for gazer in self._repo.stargazers():
-			i += 1
-		return i
+		query = '''
+		query($repo_owner:String!, $repo_name:String!) {
+			repository(owner: $repo_owner, name: $repo_name) {
+				stargazers(first: 10) {
+					totalCount
+				}
+			}
+		}'''
+		variables = {'repo_owner': self._repo_owner, 'repo_name': self._repo_name, }
+		data = self.endpoint(query, variables)
+		stars = data['data']['repository']['stargazers']['totalCount']
+		return stars
